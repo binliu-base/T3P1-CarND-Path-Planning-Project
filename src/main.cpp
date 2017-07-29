f4f84f813c0e3716157a904e5f8f00422c5d2ffc
binliu-base <2598725694@qq.com>
2017-07-30 00:10:13 +0800

    update 170729

--- src/main.cpp
+++ src/main.cpp
@@ -8,8 +8,16 @@
 #include "Eigen-3.3/Eigen/Core"
 #include "Eigen-3.3/Eigen/QR"
 #include "json.hpp"
+//added by binliu 170729
+#include "road.h"
+#include "spline.h"
+//end add
 
 using namespace std;
+
+// bool Tools::trace_tag = false;
+// string Tools::tracelog = "trace.MY.log";
+// ofstream Tools::traceStream = ofstream(Tools::tracelog.data(), ios::app);
 
 // for convenience
 using json = nlohmann::json;
@@ -159,9 +167,130 @@ vector<double> getXY(double s, double d, vector<double> maps_s, vector<double> m
 
 }
 
+//added by binliu 170729
+//all traffic in lane (besides ego) follow these speeds
+vector<int> LANE_SPEEDS = {49,49,49}; 
+
+//Number of available "cells" which should have traffic
+double TRAFFIC_DENSITY   = 0.15;
+
+// At each timestep, ego can set acceleration to value between 
+// -MAX_ACCEL and MAX_ACCEL
+int MAX_ACCEL = 2;
+
+//impacts default behavior for most states
+int SPEED_LIMIT = 49.75;
+
+// These affect the visualization
+int FRAMES_PER_SECOND = 50;
+int AMOUNT_OF_ROAD_VISIBLE = 40;
+
+int timestep = 0;
+int ourlane = -1; 
+//end add
+
+// Transform from global Cartesian x,y to local car coordinates x,y
+// where x is pointing to the positive x axis and y is deviation from the car's path
+vector<double> getLocalXY(double car_x, double car_y, double theta, double wx, double wy) {
+  vector<double> results;
+
+  // convert to local coordinates
+  float deltax = (wx - car_x);
+  float deltay = (wy - car_y);
+  results.push_back(deltax*cos(theta) + deltay*sin(theta));
+  results.push_back(-deltax*sin(theta) + deltay*cos(theta));
+  return results;
+}
+
+// segment of 25 x,y coordinates for waypoints (6 in the back, 1 closest and 18 in the front) for a given lane
+void fit_spline_segment(double car_s, vector<double> const &map_waypoints_s, vector<double> const &map_waypoints_x, vector<double> const &map_waypoints_y, vector<double> const &map_waypoints_dx, vector<double> const &map_waypoints_dy, vector<double> &waypoints_segment_s, vector<double> &waypoints_segment_s_worldSpace, vector<double> &map_waypoints_x_upsampled, vector<double> &map_waypoints_y_upsampled, vector<double> &map_waypoints_s_upsampled, vector<double> &map_waypoints_dx_upsampled, vector<double> &map_waypoints_dy_upsampled, tk::spline &spline_fit_s_to_x, tk::spline &spline_fit_s_to_y, tk::spline &spline_fit_s_to_dx, tk::spline &spline_fit_s_to_dy) {
+  // get 10 previous and 15 next waypoints
+  vector<double> waypoints_segment_x, waypoints_segment_y, waypoints_segment_dx, waypoints_segment_dy;
+  vector<int> wp_indeces;
+  const int lower_wp_i = 9;
+  const int upper_wp_i = 15;
+  int prev_wp = -1;
+  while(car_s > map_waypoints_s[prev_wp+1] && (prev_wp < (int)(map_waypoints_s.size()-1) ))
+          prev_wp++;
+  for (int i = lower_wp_i; i > 0; i--) {
+    if (prev_wp - i < 0)
+      wp_indeces.push_back(map_waypoints_s.size() + (prev_wp - i));
+    else
+      wp_indeces.push_back((prev_wp - i) % map_waypoints_s.size());
+  }
+  wp_indeces.push_back(prev_wp);
+  for (int i = 1; i < upper_wp_i; i++)
+    wp_indeces.push_back((prev_wp + i) % map_waypoints_s.size());
+
+  // FILL NEW SEGMENT WAYPOINTS
+  const double max_s = 6945.554;
+  bool crossed_through_zero = false;
+  double seg_start_s = map_waypoints_s[wp_indeces[0]];
+  for (int i = 0; i < wp_indeces.size(); i++) {
+    int cur_wp_i = wp_indeces[i];
+    waypoints_segment_x.push_back(map_waypoints_x[cur_wp_i]);
+    waypoints_segment_y.push_back(map_waypoints_y[cur_wp_i]);
+    waypoints_segment_dx.push_back(map_waypoints_dx[cur_wp_i]);
+    waypoints_segment_dy.push_back(map_waypoints_dy[cur_wp_i]);
+    // need special treatment of segments that cross over the end/beginning of lap
+    if (i > 0) {
+      if (cur_wp_i < wp_indeces[i-1])
+        crossed_through_zero = true;
+    }
+    waypoints_segment_s_worldSpace.push_back(map_waypoints_s[cur_wp_i]);
+    if (crossed_through_zero)
+      waypoints_segment_s.push_back(abs(seg_start_s - max_s) + map_waypoints_s[cur_wp_i]);
+    else
+      waypoints_segment_s.push_back(map_waypoints_s[cur_wp_i] - seg_start_s);
+  }
+  spline_fit_s_to_x.set_points(waypoints_segment_s, waypoints_segment_x);
+  spline_fit_s_to_y.set_points(waypoints_segment_s, waypoints_segment_y);
+  spline_fit_s_to_dx.set_points(waypoints_segment_s, waypoints_segment_dx);
+  spline_fit_s_to_dy.set_points(waypoints_segment_s, waypoints_segment_dy);
+
+  const int samples = int(waypoints_segment_s[waypoints_segment_s.size()-1]);
+  map_waypoints_x_upsampled.reserve(samples);
+  map_waypoints_y_upsampled.reserve(samples);
+  map_waypoints_s_upsampled.reserve(samples);
+  map_waypoints_dx_upsampled.reserve(samples);
+  map_waypoints_dy_upsampled.reserve(samples);
+  for (int i = 0; i < samples; i++) {
+    map_waypoints_x_upsampled.push_back(spline_fit_s_to_x(i));
+    map_waypoints_y_upsampled.push_back(spline_fit_s_to_y(i));
+    map_waypoints_s_upsampled.push_back(i);
+    map_waypoints_dx_upsampled.push_back(spline_fit_s_to_dx(i));
+    map_waypoints_dy_upsampled.push_back(spline_fit_s_to_dy(i));
+  }
+}
+
+  // converts world space s coordinate to local space based on provided mapping
+double get_local_s(double world_s, vector<double> const &waypoints_segment_s_worldSpace, vector<double> const &waypoints_segment_s) {
+  int prev_wp = 0;
+  // special case: first wp in list is larger than s. Meaning we are crossing over 0 somewhere.
+  // go to index with value zero first and search from there.
+  if (waypoints_segment_s_worldSpace[0] > world_s) {
+      while (waypoints_segment_s_worldSpace[prev_wp] != 0.0)
+          prev_wp += 1;
+  }
+  while ((waypoints_segment_s_worldSpace[prev_wp+1] < world_s) && (waypoints_segment_s_worldSpace[prev_wp+1] != 0))
+      prev_wp += 1;
+  double diff_world = world_s - waypoints_segment_s_worldSpace[prev_wp];
+  double s_local = waypoints_segment_s[prev_wp] + diff_world;
+  return s_local;
+}
+
+
 int main() {
   uWS::Hub h;
 
+  // set up logging
+  string log_file = "../data/logger.csv";
+  // ofstream out_log(log_file.c_str(), ofstream::out);
+  // out_log << "t,x,y,vd,xyd,nd,d,st" << endl;
+//   static ofstream out_log = ofstream(log_file.data(), ios::app);
+  static ofstream out_log = ofstream(log_file.data());
+
+  out_log << "t,x,y,yaw,laneid" << endl;
   // Load up map values for waypoint's x,y,s and d normalized normal vectors
   vector<double> map_waypoints_x;
   vector<double> map_waypoints_y;
@@ -172,6 +301,7 @@ int main() {
   // Waypoint map to read from
   string map_file_ = "../data/highway_map.csv";
   // The max s value before wrapping around the track back to 0
+  static double nextd = 6.;  
   double max_s = 6945.554;
 
   ifstream in_map_(map_file_.c_str(), ifstream::in);
@@ -196,7 +326,19 @@ int main() {
   	map_waypoints_dy.push_back(d_y);
   }
 
-  h.onMessage([&map_waypoints_x,&map_waypoints_y,&map_waypoints_s,&map_waypoints_dx,&map_waypoints_dy](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
+//added by binliu 170729
+  	static Road road = Road(SPEED_LIMIT, TRAFFIC_DENSITY, LANE_SPEEDS);
+	road.update_width = AMOUNT_OF_ROAD_VISIBLE;	  
+	int goal_s = max_s;	
+	int goal_lane = 0;	
+	int num_lanes = LANE_SPEEDS.size();
+	vector<int> ego_config = {SPEED_LIMIT,num_lanes,goal_s,goal_lane,MAX_ACCEL};	
+	road.add_ego(0,0, ego_config);	
+	static Vehicle ego = road.get_ego();
+	
+//end add	  
+
+  h.onMessage([&map_waypoints_x,&map_waypoints_y,&map_waypoints_s,&map_waypoints_dx,&map_waypoints_dy](uWS::WebSocket<uWS::SERVER> * ws, char *data, size_t length,
                      uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
     // The 4 signifies a websocket message
@@ -238,21 +380,126 @@ int main() {
           	vector<double> next_x_vals;
           	vector<double> next_y_vals;
 
+          // ###################################################  
+          // TEST - Just go straight
+          // ###################################################
+//          double dist_inc = 0.5;
+//          for(int i = 0; i < 50; i++)
+//          {
+//            next_x_vals.push_back(car_x+(dist_inc*i)*cos(deg2rad(car_yaw)));
+//            next_y_vals.push_back(car_y+(dist_inc*i)*sin(deg2rad(car_yaw)));
+//          }
+          // ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+          // END - Just go straight
+          // ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^	
+		  
 
           	// TODO: define a path made up of (x,y) points that the car will visit sequentially every .02 seconds
+			// ego.car_x = car_x;			  
+			// ego.car_y = car_y;			  
+			// ego.car_s = car_s;			  
+			// ego.car_d = car_d;			  
+			// ego.car_yaw = car_yaw;			  
+			// ego.car_speed = car_speed;			  						
+			// ego.lane = round(round(car_d-2)/4);
+
+			// figure out current lane
+			// 0: left, 1: middle, 2: right
+			ego.lane = 0;
+			if (car_d > 8) ego.lane = 2;
+			else if (car_d > 4) ego.lane = 1;
+
+			// update actual position
+			ego.set_frenet_pos(car_s, car_d);
+
+            // get last known car state
+            vector<double> prev_car_s = ego.get_s();
+            vector<double> prev_car_d = ego.get_d(); 		
+
+			// get our frenet coordinates
+			// vector<double> frenet = getFrenet(car_x, car_y, deg2rad(car_yaw), map_waypoints_x, map_waypoints_y);
+            // extract surrounding waypoints and fit a spline
+			// set up lane tracking using spline
+			// vector<vector<double>> localwps = getLocalizedWayPointSegement(car_x, car_y, car_yaw, nextd, map_waypoints_x, map_waypoints_y, map_waypoints_dx, map_waypoints_dy);			
+			
+
+			// ###################################################  
+			// PATH PLANNING 
+			// ###################################################			
+			
+			int horizon = 200;
+			bool smooth_path = previous_path_x.size() > 0;
+			int update_interval = 50; // update every second				
+			
+			// out_log << timestep << "," << ego.car_x << "," << ego.car_y << "," << ego.car_yaw << "," << ego.lane << endl;				
+			out_log << timestep 
+				<< "," << ego.car_x << "," << ego.car_y << "," << ego.car_yaw 
+				<< "," << ego.lane  
+				<< "," << prev_car_s[0]  << "," << prev_car_d[0]  
+				// << "," << localwps[0][0]  << "," << localwps[0][1]				
+				<< endl;
+
+			if (previous_path_x.size() < horizon - update_interval) {
+				out_log << endl;
+				out_log << "PATH UPDATE" << endl;
+
+				// extract surrounding waypoints and fit a spline
+				vector<double> waypoints_segment_s;
+				vector<double> waypoints_segment_s_worldSpace;
+				// TODO: Clean most (all?) of these up! Change signature of fit_spline_segment as well.
+				vector<double> map_waypoints_x_upsampled, map_waypoints_y_upsampled, map_waypoints_s_upsampled, map_waypoints_dx_upsampled, map_waypoints_dy_upsampled;
+				tk::spline spline_fit_s_to_x;
+				tk::spline spline_fit_s_to_y;
+				tk::spline spline_fit_s_to_dx;
+				tk::spline spline_fit_s_to_dy;
+				fit_spline_segment(car_s, map_waypoints_s, map_waypoints_x, map_waypoints_y, map_waypoints_dx, map_waypoints_dy, waypoints_segment_s, waypoints_segment_s_worldSpace, map_waypoints_x_upsampled, map_waypoints_y_upsampled, map_waypoints_s_upsampled, map_waypoints_dx_upsampled, map_waypoints_dy_upsampled, spline_fit_s_to_x, spline_fit_s_to_y, spline_fit_s_to_dx, spline_fit_s_to_dy);
+				
+				// convert current car_s into our local Frenet space
+				double car_local_s = get_local_s(car_s, waypoints_segment_s_worldSpace, waypoints_segment_s);
+				// convert sensor fusion data into local Frenet space
+				for (int i = 0; i < sensor_fusion.size(); i++) {
+				sensor_fusion[i][5] = get_local_s(sensor_fusion[i][5], waypoints_segment_s_worldSpace, waypoints_segment_s);
+				}
+				// turn sensor fusion data into Vehicle objects
+				vector<Vehicle> envir_vehicles(sensor_fusion.size());
+				for (int i = 0; i < sensor_fusion.size(); i++) {
+				envir_vehicles[i].set_frenet_pos(sensor_fusion[i][5], sensor_fusion[i][6]);
+				double vx = sensor_fusion[i][3];
+				double vy = sensor_fusion[i][4];
+				double velocity_per_timestep = sqrt(pow(vx, 2) + pow(vy, 2)) / 50.0;
+				envir_vehicles[i].set_frenet_motion(velocity_per_timestep, 0.0, 0.0, 0.0);
+				}
+				
+				// get last known car state
+				vector<double> prev_car_s = ego.get_s();
+				vector<double> prev_car_d = ego.get_d();            
+				// collect best guess at current car state. S position in local segment space
+				vector<double> car_state = {car_local_s, prev_car_s[1], prev_car_s[2], car_d, prev_car_d[1], prev_car_d[2]};
+			
+
+		  }				
+
+          // ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+          // END - PATH PLANNING
+          // ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+			//End TODO
+
           	msgJson["next_x"] = next_x_vals;
-          	msgJson["next_y"] = next_y_vals;
+          	msgJson["next_y"] = next_y_vals;			
 
           	auto msg = "42[\"control\","+ msgJson.dump()+"]";
 
           	//this_thread::sleep_for(chrono::milliseconds(1000));
-          	ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
+          	(*ws).send(msg.data(), msg.length(), uWS::OpCode::TEXT);
+			timestep++;        			  
+
+
           
         }
       } else {
         // Manual driving
         std::string msg = "42[\"manual\",{}]";
-        ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
+        (*ws).send(msg.data(), msg.length(), uWS::OpCode::TEXT);
       }
     }
   });
@@ -271,13 +518,13 @@ int main() {
     }
   });
 
-  h.onConnection([&h](uWS::WebSocket<uWS::SERVER> ws, uWS::HttpRequest req) {
+  h.onConnection([&h](uWS::WebSocket<uWS::SERVER> * ws, uWS::HttpRequest req) {
     std::cout << "Connected!!!" << std::endl;
   });
 
-  h.onDisconnection([&h](uWS::WebSocket<uWS::SERVER> ws, int code,
+  h.onDisconnection([&h](uWS::WebSocket<uWS::SERVER> * ws, int code,
                          char *message, size_t length) {
-    ws.close();
+    (*ws).close();
     std::cout << "Disconnected" << std::endl;
   });
 
---
+++ src/vehicle.cpp
@@ -0,0 +1,451 @@
+#include <iostream>
+#include "vehicle.h"
+#include <iostream>
+#include <math.h>
+#include <map>
+#include <string>
+#include <iterator>
+
+/**
+ * Initializes Vehicle
+ */
+Vehicle::Vehicle(int lane, int s, int v, int a) {
+
+    this->lane = lane;
+    this->s = s;
+    this->v = v;
+    this->a = a;
+    state = "CS";
+    max_acceleration = -1;
+
+}
+
+Vehicle::Vehicle() {
+  _pos_s = 0.0;
+  _vel_s = 0.0;
+  _acc_s = 0.0;
+  _pos_d = 0.0;
+  _vel_d = 0.0;
+  _acc_d = 0.0;
+}
+
+Vehicle::~Vehicle() {
+    state = "CS";    
+}
+
+
+vector<string> Vehicle::get_successor_states() {
+
+    vector<string> successor_states;
+    
+    if (state.compare("KL") == 0) {
+        successor_states.push_back("KL");
+        successor_states.push_back("PLCL");
+        successor_states.push_back("PLCR");
+    }
+
+    else if (state.compare("PLCL") == 0) {
+        successor_states.push_back("KL");
+        successor_states.push_back("PLCL");        
+        successor_states.push_back("LCL");
+    }
+
+    else if (state.compare("PLCR") == 0) {
+        successor_states.push_back("KL");
+        successor_states.push_back("PLCR");        
+        successor_states.push_back("LCR");
+    }
+
+    else if (state == "LCL") {
+        successor_states.push_back("KL");
+    }
+
+    else if (state.compare("LCR") == 0) {
+        successor_states.push_back("KL");
+    }
+
+    if (this->lane == 0) {
+        successor_states.erase(std::remove(successor_states.begin(), successor_states.end(), "PLCL"), successor_states.end());
+    }
+    
+    if (this->lane == 2) {
+        successor_states.erase(std::remove(successor_states.begin(), successor_states.end(), "PLCR"), successor_states.end());
+    }
+
+    return successor_states;    
+}
+
+
+// TODO - Implement this method.
+void Vehicle::update_state(map<int,vector < vector<int> > > predictions) {
+	/*
+    Updates the "state" of the vehicle by assigning one of the
+    following values to 'self.state':
+
+    "KL" - Keep Lane
+     - The vehicle will attempt to drive its target speed, unless there is 
+       traffic in front of it, in which case it will slow down.
+
+    "LCL" or "LCR" - Lane Change Left / Right
+     - The vehicle will IMMEDIATELY change lanes and then follow longitudinal
+       behavior for the "KL" state in the new lane.
+
+    "PLCL" or "PLCR" - Prepare for Lane Change Left / Right
+     - The vehicle will find the nearest vehicle in the adjacent lane which is
+       BEHIND itself and will adjust speed to try to get behind that vehicle.
+
+    INPUTS
+    - predictions 
+    A dictionary. The keys are ids of other vehicles and the values are arrays
+    where each entry corresponds to the vehicle's predicted location at the 
+    corresponding timestep. The FIRST element in the array gives the vehicle's
+    current position. Example (showing a car with id 3 moving at 2 m/s):
+
+    {
+      3 : [
+        {"s" : 4, "lane": 0},
+        {"s" : 6, "lane": 0},
+        {"s" : 8, "lane": 0},
+        {"s" : 10, "lane": 0},
+      ]
+    }
+
+    */
+
+    auto kl_cost = [this]() {
+        auto dl = 1.0 * (lane - goal_lane);        
+        return dl * dl;
+    };
+
+    auto plc_cost = [this](int delta_lane) {
+        double dl = (lane + 0.5 * delta_lane - goal_lane);    
+        cout << "dl " << dl << " lane " << lane << " delta_lane " << delta_lane << " goal lane " << goal_lane << endl;        
+        return dl*dl;
+    };
+
+    auto cl_cost = [this](int delta_lane, const map<int, vector<vector<int>>>& predictions) {
+
+        bool collision = false;
+        for (auto it = predictions.begin(); it != predictions.end(); ++it) {
+
+            int now = 0;
+            int v_id = it->first;
+            vector<vector<int> > v = it->second;
+            // cout << "s " << s << " lane " << lane <<  " v_id " << v_id << "  v[now][0]= "<< v[now][0] << "  v[now][1]= "<< v[now][1] <<endl;                
+            // v_id = -1 is our car
+            if (v_id != -1) {
+            
+                bool collision = (abs(lane - v[now][0]) == 1) && (abs(s - v[now][1]) <= L);
+
+                if (collision) {
+                    return 100000.0;                                    
+                }
+            }
+
+        }
+
+        auto dl = 1.0*(lane + delta_lane - goal_lane);
+        return dl*dl;
+
+    };
+
+    vector<string> successor_states = get_successor_states(); 
+
+    map<string,double> costs;
+
+    costs["KL"] = kl_cost();
+    costs["PLCL"] = plc_cost(1);
+    costs["PLCR"] = plc_cost(-1);
+    costs["LCL"] = cl_cost(1, predictions);
+    costs["LCR"] = cl_cost(-1, predictions);
+
+
+    double max_cost = 1E12;
+
+    for (auto successor_state: successor_states) {
+          
+         double successor_cost = costs[successor_state];
+         cout << "successor_state " << successor_state << " " << " successor_cost" << successor_cost << endl;
+         if (successor_cost < max_cost){
+            state = successor_state;
+            max_cost = successor_cost;
+         }
+
+    }
+
+}
+
+void Vehicle::configure(vector<int> road_data) {
+	/*
+    Called by simulator before simulation begins. Sets various
+    parameters which will impact the ego vehicle. 
+    */
+    target_speed = road_data[0];
+    lanes_available = road_data[1];
+    max_acceleration = road_data[2];
+    goal_lane = road_data[3];
+    goal_s = road_data[4];
+}
+
+string Vehicle::display() {
+
+	ostringstream oss;
+	
+	oss << "s:    " << this->s << "\n";
+    oss << "lane: " << this->lane << "\n";
+    oss << "v:    " << this->v << "\n";
+    oss << "a:    " << this->a << "\n";
+    
+    return oss.str();
+}
+
+void Vehicle::increment(int dt = 1) {
+
+	this->s += this->v * dt;
+    this->v += this->a * dt;
+}
+
+vector<int> Vehicle::state_at(int t) {
+
+	/*
+    Predicts state of vehicle in t seconds (assuming constant acceleration)
+    */
+    int s = this->s + this->v * t + this->a * t * t / 2;
+    int v = this->v + this->a * t;
+    return {this->lane, s, v, this->a};
+}
+
+bool Vehicle::collides_with(Vehicle other, int at_time) {
+
+	/*
+    Simple collision detection.
+    */
+    vector<int> check1 = state_at(at_time);
+    vector<int> check2 = other.state_at(at_time);
+    return (check1[0] == check2[0]) && (abs(check1[1]-check2[1]) <= L);
+}
+
+Vehicle::collider Vehicle::will_collide_with(Vehicle other, int timesteps) {
+
+	Vehicle::collider collider_temp;
+	collider_temp.collision = false;
+	collider_temp.time = -1; 
+
+	for (int t = 0; t < timesteps+1; t++)
+	{
+      	if( collides_with(other, t) )
+      	{
+			collider_temp.collision = true;
+			collider_temp.time = t; 
+        	return collider_temp;
+    	}
+	}
+
+	return collider_temp;
+}
+
+void Vehicle::realize_state(map<int,vector < vector<int> > > predictions) {
+   
+	/*
+    Given a state, realize it by adjusting acceleration and lane.
+    Note - lane changes happen instantaneously.
+    */
+    string state = this->state;
+    if(state.compare("CS") == 0)
+    {
+    	realize_constant_speed();
+    }
+    else if(state.compare("KL") == 0)
+    {
+    	realize_keep_lane(predictions);
+    }
+    else if(state.compare("LCL") == 0)
+    {
+    	realize_lane_change(predictions, "L");
+    }
+    else if(state.compare("LCR") == 0)
+    {
+    	realize_lane_change(predictions, "R");
+    }
+    else if(state.compare("PLCL") == 0)
+    {
+    	realize_prep_lane_change(predictions, "L");
+    }
+    else if(state.compare("PLCR") == 0)
+    {
+    	realize_prep_lane_change(predictions, "R");
+    }
+
+}
+
+void Vehicle::realize_constant_speed() {
+	a = 0;
+}
+
+int Vehicle::_max_accel_for_lane(map<int,vector<vector<int> > > predictions, int lane, int s) {
+
+	int delta_v_til_target = target_speed - v;
+    int max_acc = min(max_acceleration, delta_v_til_target);
+
+    map<int, vector<vector<int> > >::iterator it = predictions.begin();
+    vector<vector<vector<int> > > in_front;
+    while(it != predictions.end())
+    {
+       
+    	int v_id = it->first;
+    	
+        vector<vector<int> > v = it->second;
+        
+        if((v[0][0] == lane) && (v[0][1] > s))
+        {
+        	in_front.push_back(v);
+
+        }
+        it++;
+    }
+    
+    if(in_front.size() > 0)
+    {
+    	int min_s = 1000;
+    	vector<vector<int>> leading = {};
+    	for(int i = 0; i < in_front.size(); i++)
+    	{
+    		if((in_front[i][0][1]-s) < min_s)
+    		{
+    			min_s = (in_front[i][0][1]-s);
+    			leading = in_front[i];
+    		}
+    	}
+    	
+    	int next_pos = leading[1][1];
+    	int my_next = s + this->v;
+    	int separation_next = next_pos - my_next;
+    	int available_room = separation_next - preferred_buffer;
+    	max_acc = min(max_acc, available_room);
+    }
+    
+    return max_acc;
+
+}
+
+void Vehicle::realize_keep_lane(map<int,vector< vector<int> > > predictions) {
+	this->a = _max_accel_for_lane(predictions, this->lane, this->s);
+}
+
+void Vehicle::realize_lane_change(map<int,vector< vector<int> > > predictions, string direction) {
+	int delta = -1;
+    if (direction.compare("L") == 0)
+    {
+    	delta = 1;
+    }
+    this->lane += delta;
+    int lane = this->lane;
+    int s = this->s;
+    this->a = _max_accel_for_lane(predictions, lane, s);
+}
+
+void Vehicle::realize_prep_lane_change(map<int,vector<vector<int> > > predictions, string direction) {
+	int delta = -1;
+    if (direction.compare("L") == 0)
+    {
+    	delta = 1;
+    }
+    int lane = this->lane + delta;
+
+    map<int, vector<vector<int> > >::iterator it = predictions.begin();
+    vector<vector<vector<int> > > at_behind;
+    while(it != predictions.end())
+    {
+    	int v_id = it->first;
+        vector<vector<int> > v = it->second;
+
+        if((v[0][0] == lane) && (v[0][1] <= this->s))
+        {
+        	at_behind.push_back(v);
+
+        }
+        it++;
+    }
+    if(at_behind.size() > 0)
+    {
+
+    	int max_s = -1000;
+    	vector<vector<int> > nearest_behind = {};
+    	for(int i = 0; i < at_behind.size(); i++)
+    	{
+    		if((at_behind[i][0][1]) > max_s)
+    		{
+    			max_s = at_behind[i][0][1];
+    			nearest_behind = at_behind[i];
+    		}
+    	}
+    	int target_vel = nearest_behind[1][1] - nearest_behind[0][1];
+    	int delta_v = this->v - target_vel;
+    	int delta_s = this->s - nearest_behind[0][1];
+    	if(delta_v != 0)
+    	{
+
+    		int time = -2 * delta_s/delta_v;
+    		int a;
+    		if (time == 0)
+    		{
+    			a = this->a;
+    		}
+    		else
+    		{
+    			a = delta_v/time;
+    		}
+    		if(a > this->max_acceleration)
+    		{
+    			a = this->max_acceleration;
+    		}
+    		if(a < -this->max_acceleration)
+    		{
+    			a = -this->max_acceleration;
+    		}
+    		this->a = a;
+    	}
+    	else
+    	{
+    		int my_min_acc = max(-this->max_acceleration,-delta_s);
+    		this->a = my_min_acc;
+    	}
+
+    }
+
+}
+
+vector<vector<int> > Vehicle::generate_predictions(int horizon = 200) {
+
+	vector<vector<int> > predictions;
+    for( int i = 0; i < horizon; i++)
+    {
+      vector<int> check1 = state_at(i);
+      vector<int> lane_s = {check1[0], check1[1]};
+      predictions.push_back(lane_s);
+  	}
+    return predictions;
+
+}
+
+// Added by binliu 170729
+void Vehicle::set_frenet_pos(double pos_s, double pos_d) {
+  _pos_s = pos_s;
+  _pos_d = pos_d;
+}
+
+vector<double> Vehicle::get_s() const {
+  return {_pos_s, _vel_s, _acc_s};
+}
+
+vector<double> Vehicle::get_d() const {
+  return {_pos_d, _vel_d, _acc_d};
+}
+
+void Vehicle::set_frenet_motion(double vel_s, double acc_s, double vel_d, double acc_d) {
+  _vel_s = vel_s;
+  _acc_s = acc_s;
+  _vel_d = vel_d;
+  _acc_d = acc_d;
+}
+//end add
